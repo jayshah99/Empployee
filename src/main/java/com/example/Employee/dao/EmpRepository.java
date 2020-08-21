87f6328925d8a20afd7d3b2707918f92fdbec559
jayshah99 <69785767+jayshah99@users.noreply.github.com>
2020-08-21 14:51:52 +0530

    Delete EmpRepository.java

--- src/main/java/com/example/Employee/dao/EmpRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Employee.dao;
-
-import com.example.Employee.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpRepository extends JpaRepository <Employee, Integer>{
-}
